909c3c5eb37c17907b80845f8dfe12d293eeddae
Kirill Lagutin <lagutin.k@mail.ru>
2022-05-24 20:39:30 +0300

    Init DataGrid

---
+++ Staffer.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Windows;
+
+namespace Staffer.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Lib.Staffer> Staffers { get; set; }
+
+        public MainWindow()
+        {
+            Staffers = new ObservableCollection<Lib.Staffer>(InitStaffers());
+            
+            InitializeComponent();
+        }
+        
+        private List<Lib.Staffer> InitStaffers()
+        {
+            var url = File.ReadAllText("urls.txt");
+            using var webStream = WebRequest.Create(url).GetResponse().GetResponseStream();
+            using var stream = new StreamReader(webStream);
+            var json = stream.ReadToEnd();
+            return JsonSerializer.Deserialize<List<Lib.Staffer>>(json);
+        }
+    }
+}
--- Staffer.Lib/Staffer.cs
+++ Staffer.Lib/Staffer.cs
@@ -1,11 +1,24 @@
-﻿namespace Staffer.Lib;
+﻿using System.Text.Json.Serialization;
+
+namespace Staffer.Lib;
 
 public record Staffer
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+    
+    [JsonPropertyName(("firstName"))]
     public string FirstName { get; set; }
+    
+    [JsonPropertyName(("lastName"))]
     public string LastName { get; set; }
+    
+    [JsonPropertyName(("dateOfBirth"))]
     public DateTime DateOfBirth { get; set; }
+    
+    [JsonPropertyName(("department"))]
     public string Department { get; set; }
+    
+    [JsonPropertyName(("position"))]
     public string Position { get; set; }
 }
